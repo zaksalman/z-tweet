6a17650850cb20ee3876f2aa8cc1e13b46a989f9
jojozon <jojozon@naver.com>
2024-01-25 00:41:23 +0900

    nothing

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 https://github.com/ZeroCho/next-app-router-z/
 
 인프런 강좌!~ z 따라하기
+
+진행중 상황 : 메인 페이지//// URL /
---
+++ src/app/(beforeLogin)/page.tsx
@@ -0,0 +1,8 @@
+import Main from "./_component/Main";
+
+
+export default function Home() {
+    return (
+        <Main/>
+    )
+}
